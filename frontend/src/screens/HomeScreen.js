cab687cd2c4c19004136f2d50e5e7d6a48b1714f
Azamat <so.fly.azash@gmail.com>
2021-08-13 12:01:03 +0600

    fetched data from backend, added loading and message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "../components/Product";
-import data from "../data";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
-function HomeScreen() {
+export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data.products); // iters an array, not an obj
+      } catch (err) {
+        setError(err.message); // if there any message
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  // console.log(products);
   return (
     <div>
-      <div className="row center">
-        {data.products.map((product) => (
-          <Product key={product._id} product={product}></Product>
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product}></Product>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
-export default HomeScreen;

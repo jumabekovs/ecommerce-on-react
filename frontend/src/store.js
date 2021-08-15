de829d4b1f92b06285ae552af48db1a4e797d7c9
Azamat <so.fly.azash@gmail.com>
2021-08-15 19:01:24 +0600

    added badge for cartItems

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,24 @@
 /* creating redux store */
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
 import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
 
-const initialState = {};
+/* initial state takes products from local storage, if nothing there, shows empty array */
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
+};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 /* shows in Chrome extension */
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
